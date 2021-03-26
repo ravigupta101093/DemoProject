67c41a9bf36a9c64e49d62f2193c390a899dac8e
Nicky Singh <nickysingh@Agicents-Mac-mini-2.local>
2021-03-26 11:28:58 +0530

    Demo

---
+++ MvvmDemoNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MvvmDemoNew
+//
+//  Created by Nicky Singh on 26/03/21.
+//  Copyright © 2021 agicent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
